b3272c223a009d37cfa8ed6113b84da6fffdb1be
langan.kg Langan <langan.code@gmail.com>
2020-05-05 10:03:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ if index.between?(0,8) && !position_taken?(board, index)
 else 
   return false
 end
-
+end
 def position_taken?(board, index)
    if board[index] != " " || board[index] != "" || board[index] != niL 
      return false
