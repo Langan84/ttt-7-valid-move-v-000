4a168179aada84340dbc61bb64e0184194bce33a
langan.kg Langan <langan.code@gmail.com>
2020-05-05 09:50:31 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ index.between?(0,8) && !position_taken?(board, index)
 
 
 if position_taken?(board, index)
-   board[index] != " " || board[index] != "" || board[index] != niL false 
+   board[index] != " " || board[index] != "" || board[index] != niL 
    else [index]!= "X" || board[index] != "O"
    false
  end
