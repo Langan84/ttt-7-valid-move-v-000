6483fb8cc9fcc90ea3542fb68c946c68048c0f64
langan.kg Langan <langan.code@gmail.com>
2020-05-05 09:49:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@ index.between?(0,8) && !position_taken?(board, index)
 
 if position_taken?(board, index)
    board[index] != " " || board[index] != "" || board[index] != niL false 
-   [index]!= "X" || board[index] != "O"
+ else  [index]!= "X" || board[index] != "O"
    false
  end
  end
