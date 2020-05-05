d6ef62c6726c6f1782f42a2c8df6d5b94b31ac23
langan.kg Langan <langan.code@gmail.com>
2020-05-05 09:47:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-index.between?(0,8) && !position_taken?(board, index) || board[index]== "X" || board[index] == "O"
+index.between?(0,8) && !position_taken?(board, index) || board[index]!= "X" || board[index] != "O"
   true 
 end
 
