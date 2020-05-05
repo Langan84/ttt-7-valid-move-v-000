2ec7c22b6ab5d5bf64604b9df4b05f598d1c2d50
langan.kg Langan <langan.code@gmail.com>
2020-05-05 09:45:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-if  index.between?(0,8) && !position_taken?(board, index)
-  elsif board[index]== "X" || board[index] == "O"
-  return authenticate_or_request_with_http_basic
+index.between?(0,8) && !position_taken?(board, index) || board[index]== "X" || board[index] == "O"
+ authenticate_or_request_with_http_digest
 end
 end
 
