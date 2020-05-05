70d6cdb4afb30061c87db91e2002e9c6ffc2be6e
langan.kg Langan <langan.code@gmail.com>
2020-05-05 10:09:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,19 +1,19 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
   if index.between?(0,8) && !position_taken?(board, index) 
-  return true 
+  true 
   else 
-  return false
+  false
   end
 end
 def position_taken?(board, index)
    if board[index] != " " || board[index] != "" || board[index] != niL 
-    return false
+    false
      
      elsif board[index]== "X" || board[index] == "O"
-    return true
+    true
     
     else 
-    return false
+    false
     end
  end
