065e5378ed0a63c6141990972a6bfe2ad6f89252
langan.kg Langan <langan.code@gmail.com>
2020-05-05 09:43:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board,index)
 if  index.between?(0,8) && !position_taken?(board, index)
   elsif board[index]== "X" || board[index] == "O"
-  return true
+  return authenticate_or_request_with_http_basic
 end
 end
 
