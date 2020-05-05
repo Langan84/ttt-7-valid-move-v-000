761acfe0baf4ab4fd4ce1dc3e3d5a9379db1a11e
langan.kg Langan <langan.code@gmail.com>
2020-05-05 10:16:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def valid_move?(board,index)
 end
 
 def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == niL) 
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil) 
     false
      
      elsif (board[index] == "X") || (board[index] == "O")
